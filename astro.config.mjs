31933c23919baa2141a9b87872d8fb4aae2fd197
KannannB <kannann.bhanlin@gmail.com>
2026-06-19 15:39:21 +0200

    mise a jour domaine afoc34.fr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://afoc34.netlify.app',
+  site: 'https://afoc34.fr',
   integrations: [sitemap()],
   output: 'static',
 });
